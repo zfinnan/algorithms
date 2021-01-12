b17578d8143775dbfd5f8d3fa22f79e2771f41d3
zfinnan <63361320+zfinnan@users.noreply.github.com>
2021-01-12 09:09:07 -0500

    formatting

--- merge_sort.py
+++ merge_sort.py
@@ -3,15 +3,11 @@ def mergeSort(arr):
         mid = len(arr) // 2
         left = arr[:mid]
         right = arr[mid:]
-
         mergeSort(left)
         mergeSort(right)
-
         i = 0
         j = 0
-        
         k = 0
-        
         while i < len(left) and j < len(right):
             if left[i] < right[j]:
               arr[k] = left[i]
@@ -20,17 +16,14 @@ def mergeSort(arr):
                 arr[k] = right[j]
                 j += 1
             k += 1
-
         while i < len(left):
             arr[k] = left[i]
             i += 1
             k += 1
-
         while j < len(right):
             arr[k]=right[j]
             j += 1
             k += 1
-
 arr = [54,26,93,17,77,31,44,55,20]
 mergeSort(arr)
 print(arr)
